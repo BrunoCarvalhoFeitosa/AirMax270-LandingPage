8ca4e4bfa1eaeafe93ef4f425a52f0554290a33d
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2020-01-26 18:44:11 -0300

    Adjust in name of function scrollLoader

--- src/js/jquery.scrollWindow.js
+++ src/js/jquery.scrollWindow.js
@@ -1,8 +1,8 @@
-function myFunction() {
+function scrollLoader() {
     var e = 
     (document.body.scrollTop || document.documentElement.scrollTop) / 
     (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
     document.getElementById("page-ProgressBar").style.width = e + "%";
 }
 
-window.onscroll = function() { myFunction() };
+window.onscroll = function() { scrollLoader() };
